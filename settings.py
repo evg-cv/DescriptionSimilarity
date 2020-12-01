d710e6266338eea71ef5a4a8c160db8507ce38b7
evg-cv <evgenijkolotov333@gmail.com>
2020-12-01 06:26:07 -0300

    Complete

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(CUR_DIR, 'utils', 'model', 'pruned.word2vec.txt')
 OUTPUT_DIR = make_directory_if_not_exists(os.path.join(CUR_DIR, 'output'))
 
-INPUT_EXCEL_PATH = "/media/main/Data/Task/DescriptionSimilarity/Input.xlsx"
+INPUT_EXCEL_PATH = ""
 SIMILARITY_THRESH = 0.3
 SIMILARITY_NUMBER = 3
